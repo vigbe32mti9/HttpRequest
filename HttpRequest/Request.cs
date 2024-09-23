41f17bee2d5bc2ad6022d5ff6de4b2e661f48bab
vigbe32mti9 <vigbe32m@ti9.ir>
2024-09-23 19:07:51 +0330

    Update Request.cs
    add

--- HttpRequest/Request.cs
+++ HttpRequest/Request.cs
@@ -10,7 +10,7 @@ public class Request
     public void SetParam(object param) => Param = param;
     public void SetToken(string token) => Token = token;
 
-    public HttpMethod Method { get; }
+    public HttpMethod Method { get; set; }
     public string Url { get; }
     public object? Param { get; private set; }
     public string? Token { get; private set; }
